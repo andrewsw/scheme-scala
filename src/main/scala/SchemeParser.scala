ae9ce2ef174e22020f88495e4df61d3e7feedc6e
Andrew Sackville-West <andrew@swclan.homelinux.org>
2013-01-27 20:18:39 -0800

    get things going: README, more .gitignore, sbt build stuff, first bit of parsing
    mostly getting things set up, and a little bit of the first section
    done: parsing a single character from the "symbol" set and reports
    success of failure.

---
+++ src/main/scala/SchemeParser.scala
@@ -0,0 +1,30 @@
+package parser
+
+import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.input.CharSequenceReader
+import scala.collection.immutable.StringOps
+
+object SchemeParser extends Parsers {
+
+  type Elem = Char
+
+  def oneOf(a:String) = {
+    val accepted = new StringOps(a)
+    acceptIf(c => (accepted.toList.exists(_ == c)))(c => c + " not oneOf " + a)
+  }
+
+  def symbol:Parser[Char] = oneOf("!#$%&|*+-/:<=>?@^_~")
+
+  def parse (s:String) = {
+    val input = new CharSequenceReader(s)
+    symbol(input)
+  }
+
+  def test (s:String) {
+    parse(s) match {
+      case Success(result, _) => println("parsed " + result)
+      case e:NoSuccess => println("parsing failed on " + s)
+    }
+  }
+}
+
